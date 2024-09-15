6ee0ace5d1b5a74829ec42cf75ec0b9fc77217d4
Andrea01101 <162194766+Andrea01101@users.noreply.github.com>
2024-09-15 11:34:29 +0200

    Update controller.py

--- UI/controller.py
+++ UI/controller.py
@@ -11,9 +11,35 @@ class Controller:
         self._listShape = []
 
     def fillDD(self):
-        pass
+        years = self._model.getAllYears()
+        shapes = self._model.getAllShapes()
+
+        for a in years:
+            self._view.ddyear.options.append(ft.dropdown.Option(a))
+        for s in shapes:
+            self._view.ddshape.options.append(ft.dropdown.Option(s))
+
+        self._view.update_page()
 
     def handle_graph(self, e):
-        pass
+        s = self._view.ddshape.value
+        y = self._view.ddyear.value
+        self._model.buildGraph(s, y)
+        n = self._model.getNodes()
+        e = self._model.getEdges()
+
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(ft.Text("Grafo correttamente creato"))
+        self._view.txt_result.controls.append(ft.Text(f"Numero vertici: {n}, numero archi: {e}"))
+
+        listaV = self._model.getWeight()
+        for v, tot in listaV:
+            self._view.txt_result.controls.append(ft.Text(f"Nodo: {v}, somma pesi su archi = {tot}"))
+
+
+        self._view.update_page()
+
+
+
     def handle_path(self, e):
         pass
