08dfd712d7343c2e8b0f84a1211deb71408d7242
Andrea01101 <162194766+Andrea01101@users.noreply.github.com>
2024-09-15 11:35:38 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,3 +1,45 @@
+import networkx as nx
+
+from database.DAO import DAO
+
+
 class Model:
     def __init__(self):
-        pass
+        self._grafo = nx.Graph()
+        self._states = DAO.getAllStates()
+        self._idMap = {}
+        for s in self._states:
+            self._idMap[s.id] = s
+
+    def buildGraph(self, s, y):
+
+        self._grafo.add_nodes_from(self._states)
+
+        neight = DAO.getAllEdges(s, y, self._idMap)
+        for n in neight:
+            self._grafo.add_edge(n.state1, n.state2, weight=n.tot)
+
+    def getWeight(self):
+        lista = []
+        for v in self._grafo.nodes:
+            somma = 0
+            for n in list(self._grafo.neighbors(v)):
+                somma += self._grafo[v][n]["weight"]
+            lista.append((v, somma))
+        return lista
+
+
+
+    def getAllYears(self):
+        years = list(set([date.year for date in DAO.getAllYears()]))
+        return years
+
+    def getAllShapes(self):
+        return DAO.getAllShapes()
+
+    def getNodes(self):
+        return len(self._grafo.nodes)
+
+    def getEdges(self):
+        return len(self._grafo.edges)
+
